6beeab6f496ff56038445b804cdb9fbfc2dbd382
Pranav Pailkar <44741353+pranavpkr@users.noreply.github.com>
2021-12-17 01:05:16 -0500

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Dec  4 23:00:19 2021
-
 @author: pranav
 """
 import os, sys,re
 
-# filename = 'WeddingOfFish_v2'
 # Reading filename to use in key
 filename = os.environ['map_input_file']
 filename = filename.split('/')[-1].split('.')[0]
 # print(filename, 1)
 
-# loading english dictionary downloaded from github
+# loading english dictionary
 english_words ={}
 with open('D:\Data\words_alpha.txt', 'r') as f:
     english_words = set(f.read().split())
@@ -22,6 +19,6 @@ for line in sys.stdin:
     # keeping only words,space
     line = re.sub('[^\w ]+', '', line.strip())
     
-    # iterate each word and give 1 if non-english else 0
+    # iterate each word and give 1 if non-english else 0, along with filename as key
     for word in line.split():
         print(filename, 0 if word.lower() in english_words else 1)
